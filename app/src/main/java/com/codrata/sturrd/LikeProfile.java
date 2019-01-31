0c8c8ec7212e1cc1b20379d84e356c8d2e73add1
codrata <clintonoye@codrata.com>
2019-01-31 10:45:25 +0100

    like to matches configured

--- app/src/main/java/com/codrata/sturrd/LikeProfile.java
+++ app/src/main/java/com/codrata/sturrd/LikeProfile.java
@@ -2,10 +2,14 @@ package com.codrata.sturrd;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -25,9 +29,10 @@ public class LikeProfile extends AppCompatActivity {
             mAge;
 
     private ImageView mImage;
-    private String likeId, name, age, job, about, profileImageUrl;
+    private String likeId, name, age, job, about, profileImageUrl, currentUid;
+    private FirebaseAuth mAuth;
 
-    DatabaseReference mDatabaseUser;
+    DatabaseReference mDatabaseUser, mUserConnection;
 
 
     @Override
@@ -36,7 +41,7 @@ public class LikeProfile extends AppCompatActivity {
         setContentView(R.layout.activity_like_profile);
 
         likeId = getIntent().getExtras().getString("likeId");
-
+        mAuth = FirebaseAuth.getInstance();
 
         mName = findViewById(R.id.like_name_profile);
         mJob = findViewById(R.id.like_job_profile);
@@ -45,61 +50,95 @@ public class LikeProfile extends AppCompatActivity {
         mAge = findViewById(R.id.like_age_profile);
 
         mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(likeId);
+        mUserConnection = FirebaseDatabase.getInstance().getReference().child("Users");
+        currentUid = mAuth.getCurrentUser().getUid();
         getUserInfo();
 
+        FloatingActionButton fabNopeProfile = findViewById(R.id.fabNopeProfile);
+        FloatingActionButton fabLikeProfile = findViewById(R.id.fabLikeProfile);
+
+        fabNopeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mUserConnection.child(likeId).child("connections").child("nopes").child(currentUid).setValue(true);
+            }
+        });
+
     }
 
+    private void getUserInfo() {
+        mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
 
-        private void getUserInfo() {
-            mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
-
-                            if (dataSnapshot.child("name").exists()) {
-                                name = dataSnapshot.child("name").getValue().toString();
-                                if (dataSnapshot.child("age").exists()) {
-                                    age = dataSnapshot.child("age").getValue().toString();
-                                    mName.setText(name + ",");
-                                    mAge.setText(age);
-                                }else {
-                                    mName.setText(name + ",");
-                                    mAge.setText("");
-                                }
-                            }else {
-                                mName.setText(("No Name"));
-                            }
-
-
-
-                            if (dataSnapshot.child("job").exists()) {
-                                job = dataSnapshot.child("job").getValue().toString();
-                                mJob.setText(job);
-                            }else {
-                                mJob.setText("No Job");
-                            }
-
-                            if (dataSnapshot.child("about").exists()) {
-                                about = dataSnapshot.child("about").getValue().toString();
-                                mAbout.setText("About:" + "\n" + "\n"  + about);
-                            }else {
-                                mAbout.setText("No About");
-                            }
-                            profileImageUrl = dataSnapshot.child("profileImageUrl").getValue().toString();
-
-
-                        if (!profileImageUrl.equals("default"))
-                            Glide.with(getApplicationContext()).load(profileImageUrl).into(mImage);
+                    if (dataSnapshot.child("name").exists()) {
+                        name = dataSnapshot.child("name").getValue().toString();
+                        if (dataSnapshot.child("age").exists()) {
+                            age = dataSnapshot.child("age").getValue().toString();
+                            mAge.setText(age);
+                        } else {
+                            mName.setText(name + ",");
+                            mAge.setText("");
+                        }
+                    } else {
+                        mName.setText(("No Name"));
                     }
+
+                    if (dataSnapshot.child("job").exists()) {
+                        job = dataSnapshot.child("job").getValue().toString();
+                        mJob.setText(job);
+                    } else {
+                        mJob.setText("No Job");
+                    }
+
+                    if (dataSnapshot.child("about").exists()) {
+                        about = dataSnapshot.child("about").getValue().toString();
+                        mAbout.setText(about);
+                    } else {
+                        mAbout.setText("No About");
+                    }
+                    profileImageUrl = dataSnapshot.child("profileImageUrl").getValue().toString();
+
+
+                    if (!profileImageUrl.equals("default"))
+                        Glide.with(getApplicationContext()).load(profileImageUrl).into(mImage);
                 }
+            }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                }
-            });
-
-        }
+            }
+        });
 
     }
 
+    public void onClick(View view) {
+        mUserConnection.child(likeId).child("connections").child("likes").child(currentUid).setValue(true);
+        DatabaseReference currentUserConnectionsDb = mUserConnection.child(currentUid).child("connections").child("likes").child(likeId);
+        currentUserConnectionsDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(getApplicationContext(), "new Connection", Toast.LENGTH_LONG).show();
+
+                    String key = FirebaseDatabase.getInstance().getReference().child("Chat").push().getKey();
+
+                    mUserConnection.child(dataSnapshot.getKey()).child("connections").child("matches").child(currentUid).child("ChatId").setValue(key);
+                    mUserConnection.child(currentUid).child("connections").child("matches").child(dataSnapshot.getKey()).child("ChatId").setValue(key);
+
+                    SendNotification sendNotification = new SendNotification();
+                    sendNotification.SendNotification("check it out!", "new Connection!", dataSnapshot.getKey());
+
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+}
